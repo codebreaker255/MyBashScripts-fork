dbec0cf93a01cd4fd92119712ed90b8688e873a1
pr4xis-1111 <pragnaysharma@gmail.com>
2026-03-13 11:04:44 +0530

    Create systeminfo.sh
    Simple system information declare script

---
+++ systeminfo.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+echo "Operating system: $(cat /etc/os-release | cut -d "=" -f2 | head -n 1)"
+
+echo "Kernel Version: $(uname -r)"
+
+echo "Hardware Type: $(uname -p)"
+
+echo "System Name: $( uname -a |cut -d " " -f2)"
